542d0565c584634f703bcc7299338e4f21443f55
Nobel Suhendra <71556513+NobelSuhendra@users.noreply.github.com>
2022-08-22 19:25:31 +0700

    Create prog.cpp

---
+++ prog.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define fr first 
+#define sc second
+
+#define inc(t, a, b, c) for (t = a; t < b; t += c)
+#define dec(t, a, b, c) for (t = a; t > b; t -= c)
+#define dbg(a) cout << " >> " << a << endl
+
+using namespace std;
+
+ll mn = INT8_MIN;
+ll mx = INT8_MAX;
+string yes = "YES";
+string no = "NO";
+
+ll i, j, h;
+ll t = 1;
+
+void run();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // cin >> t;
+    while(t--) run();
+}
+
+void run() {
+    dbg("START");
+    // CODE
+}
